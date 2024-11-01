98136a71b418fb6cd7aef28b98d3da2bf7dcbc89
Lyuboslav Nalbantov <tails969@gmail.com>
2024-11-01 11:14:56 +0100

    First commit

---
+++ Assets/_Scripts/GyroControl.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class GyroControl : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private bool gyroEnabled;
+}
