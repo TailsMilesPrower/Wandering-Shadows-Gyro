4a7c5b26750d87d6c3cf26653c189c25b39576f7
Lyuboslav Nalbantov <tails969@gmail.com>
2024-11-01 11:36:14 +0100

    Changes to Settings and Script

--- Assets/_Scripts/GyroControl.cs
+++ Assets/_Scripts/GyroControl.cs
@@ -4,4 +4,35 @@ public class GyroControl : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private bool gyroEnabled;
+    private Gyroscope gyro;
+
+    private GameObject cameraContainer;
+    private Quaternion rotation;
+
+    private void Start()
+    {
+        cameraContainer = new GameObject("Camera Container");
+        cameraContainer.transform.position = transform.position;
+        transform.SetParent(cameraContainer.transform);
+        
+        gyroEnabled = EnableGyro();
+    }
+
+    private bool EnableGyro()
+    {
+        if (SystemInfo.supportsGyroscope)
+        {
+            gyro = Input.gyro;
+            gyro.enabled = true;
+            return true;
+        }
+
+        return false;
+    }
+
+    private void Update()
+    {
+        //gyro.attitude = Time.deltaTime;
+    }
+
 }
