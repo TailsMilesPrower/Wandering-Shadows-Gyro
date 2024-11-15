6c6920ff9563a4bdf53107541077e15ba229528f
Lyuboslav Nalbantov <tails969@gmail.com>
2024-11-15 12:18:59 +0100

    More new assets for testing

--- Assets/Scripts/Gyro/GyroControl.cs
+++ Assets/Scripts/Gyro/GyroControl.cs
@@ -5,16 +5,23 @@ public class GyroControl : MonoBehaviour
     private bool gyroEnabled;
     private Gyroscope gyro;
 
-    private GameObject cameraContainer;
+    //private GameObject cameraContainer;
+    private GameObject Camera_FollowsPlayer_Prefab;
+    public Camera cam;
+
     private Quaternion rotation;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
-        cameraContainer = new GameObject("Camera Container");
-        cameraContainer.transform.position = transform.position;
-        transform.SetParent(cameraContainer.transform);
-        
+        GameObject gameObject = Instantiate(Camera_FollowsPlayer_Prefab);
+        Camera_FollowsPlayer_Prefab.transform.position = transform.position;
+        transform.SetParent(Camera_FollowsPlayer_Prefab.transform);
+
+        //cameraContainer = new GameObject("Camera Container");
+        //cameraContainer.transform.position = transform.position;
+        //transform.SetParent(cameraContainer.transform);
+
         gyroEnabled = EnableGyro();
     }
 
@@ -24,8 +31,9 @@ public class GyroControl : MonoBehaviour
         {
             gyro = Input.gyro;
             gyro.enabled = true;
-
-            cameraContainer.transform.rotation = Quaternion.Euler(60f, 180f, 0f);
+            
+            Camera_FollowsPlayer_Prefab.transform.rotation = Quaternion.Euler(60f, 180f, 0f);
+            //cameraContainer.transform.rotation = Quaternion.Euler(60f, 180f, 0f);
             rotation = new Quaternion(0, 0, 1, 0);
 
             return true;
---
+++ Assets/Scripts/Gyro/ScreenRotation.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class ScreenRotation : MonoBehaviour
+{
+    public static ScreenRotation orientation;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
